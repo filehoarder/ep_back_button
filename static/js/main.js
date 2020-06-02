4f4558096df6dd1480c0aaa33975745c6f4d9caa
Gerrit Pape <papeg@mail.upb.de>
2020-06-02 16:28:45 +0200

    use the home icon

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 exports.postAceInit = function(hook_name, args) {
-    var backButton = '<li class="ep_back_button-button"><a onclick="window.location = window.location.origin;" title=""><span class="buttonicon buttonicon-undo-alt"></span></a></li>';
+    var backButton = '<li class="ep_back_button-button" data-type="home" data-key="home"><a onclick="window.location = window.location.origin;" title=""><span class="buttonicon buttonicon-home"></span></a></li>';
 
     $("#editbar").contents().find("li[data-key='bold']").before(backButton);
 }
